29a27b9de02dfbde9212b18b05a88f28eddff0e8
Alexander Braatz <alex.braatz.g@gmail.com>
2020-08-04 15:09:04 +0100

    finished Class challenge 01

---
+++ 04-classes/example.php
@@ -0,0 +1,16 @@
+<?php
+
+
+require __DIR__ . "/vendor/autoload.php";
+
+
+class Person{
+    private $firstName;
+    private $lastName;
+    private $dateOfBirth;
+    private $nrOfLegs = 2; // this is a default value of 2 , used as most instances of person will have 2 legs
+    public function __constructor($firstName, $lastNames, $dateOfBirth , $dateOfBirth)
+    {
+        $this->first
+    }
+}
