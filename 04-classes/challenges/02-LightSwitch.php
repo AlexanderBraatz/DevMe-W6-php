7b732198e24220b7fa91b68d28effc3e2fdb564c
Alexander Braatz <alex.braatz.g@gmail.com>
2020-08-04 15:17:17 +0100

    finished Class challenge 02

---
+++ 04-classes/challenges/02-LightSwitch.php
@@ -0,0 +1,43 @@
+<?php
+//Create a class that represents a light switch
+
+require __DIR__ . "/vendor/autoload.php";
+
+class LightSwitch 
+{
+    private $state = false;
+
+    public function __contruct($initialState)
+    {
+        $this->state = $initialState;
+    }
+
+    public function isOn()
+    {
+        return $this->state;
+    }
+
+    public function turnOn()
+    {
+         $this->state = true;
+    }
+
+    public function turnOff()
+    {
+        $this->state = false;
+    }
+
+}
+
+$lightSwitch = new LightSwitch();
+
+// you can check whether it is on or not
+dump($lightSwitch->isOn()); // false
+
+// you can turn it on
+$lightSwitch->turnOn();
+dump($lightSwitch->isOn()); // true
+
+// you can turn it off
+$lightSwitch->turnOff();
+dump($lightSwitch->isOn()); // false
