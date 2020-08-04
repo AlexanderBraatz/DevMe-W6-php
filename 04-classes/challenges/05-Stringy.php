32fd7300908a3c2e2a57e8c0b1c20f9c0f9f0790
Alexander Braatz <alex.braatz.g@gmail.com>
2020-08-04 16:25:32 +0100

    finished Class challenge 05

---
+++ 04-classes/challenges/05-Stringy.php
@@ -0,0 +1,52 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+// Create a class that lets you do things with a string.
+// Hint: you might want to look at the PHP string functions
+
+
+class Stringy 
+{
+    private $string;
+
+    public function __construct($string){
+        $this->string = $string;
+    }
+
+    public function lower()
+    { 
+        return strtolower($this->string);
+    }
+
+    public function upper()
+    {
+        return strtoupper($this->string);
+    }
+
+    public function append($str)
+    {
+        return "{$this->string}{$str}";
+    }
+
+    public function repeat($times)
+    {
+      return str_repeat("{$this->string}", $times);
+        
+    }
+}
+
+
+$string = new Stringy("Na");
+
+// it can lowercase a string
+dump($string->lower()); // "na"
+
+// it can uppercase a string
+dump($string->upper()); // "NA"
+
+// it can concatenate
+dump($string->append("blah")); // "Nablah"
+
+// it can repeat a string
+dump($string->repeat(5)); // "NaNaNaNaNa"
