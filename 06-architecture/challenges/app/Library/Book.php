eaa1ea758b74a59b84454e9c4aa967b19b698b53
Alexander Braatz <alex.braatz.g@gmail.com>
2020-08-05 20:07:30 +0100

    finished 06-architecture challenge 4

---
+++ 06-architecture/challenges/app/Library/Book.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Library;
+    
+
+class Book
+{
+    private $title; 
+    private $numberOfPages;
+    private $bookmark = 1;
+
+    public function __construct($title, $numberOfPages)
+    {
+        $this->title = $title;
+        $this->numberofPages = $numberOfPages;
+    }
+
+    public function read($progress)
+    {
+        $this->bookmark += $progress;
+        return $this;
+    }
+
+    public function currentPage()
+    {
+        return $this->bookmark;
+    }
+
+}
